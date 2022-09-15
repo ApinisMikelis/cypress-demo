e907fa5a583d7a8ae4c341a59f500fc89a5ce417
Miķelis Apinis <mikelis.apinis@try.no>
2022-09-15 20:41:08 +0300

    Setup todo loading and display on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Todo, TodoModel } from './components/todo/todo';
+import { supabase } from './supabaseClient';
+import { PostgrestError } from '@supabase/supabase-js';
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<PostgrestError>();
+  const [todos, setTodos] = useState<TodoModel[]>([]);
+
+  useEffect(() => {
+    loadTodos().then((todos) => setTodos(todos));
+  }, []);
+
+  const loadTodos = async (): Promise<TodoModel[]> => {
+    let serverTodos: TodoModel[] = [];
+
+    try {
+      setLoading(true);
+
+      let { data, error, status } = await supabase
+        .from('todos')
+        .select(`id, title, description, done, created_at`);
+
+      if (error && status !== 406) {
+        setError(error);
+      }
+
+      if (data) {
+        serverTodos = data;
+      }
+    } finally {
+      setLoading(false);
+    }
+
+    return serverTodos;
+  };
+
   return (
     <div className="App">
-
+      {loading && <p>loading...</p>}
+      {error && <p>{error.message}</p>}
+      {todos.length > 0 &&
+        todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
     </div>
   );
 }
