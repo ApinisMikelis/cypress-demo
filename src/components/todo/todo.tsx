53ff16662ab94866038ae25c623d96bf1020ec7d
Miķelis Apinis <mikelis.apinis@try.no>
2022-09-20 18:41:33 +0300

    Basic DELETE implemented

--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { supabase } from '../../supabaseClient';
 
 export interface TodoModel {
   id: number;
@@ -13,12 +14,34 @@ type Props = {
 };
 
 export const Todo: FC<Props> = ({ todo }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const deleteTodo = async (): Promise<any> => {
+    try {
+      setLoading(true);
+
+      const { status } = await supabase
+        .from('todos')
+        .delete()
+        .match({ id: todo.id });
+
+      return status;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="todo">
       <p>{todo.done ? 'done' : 'to be done'}</p>
       <h2>{todo.title}</h2>
       <p>{todo.description}</p>
       <time>{todo.created_at}</time>
+      {!loading && (
+        <button type="button" onClick={deleteTodo}>
+          Delete
+        </button>
+      )}
     </div>
   );
 };
